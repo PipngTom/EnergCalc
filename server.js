4675173ed783c31d50b09be327f9fa35ed4fbb40
PipngTom <ivangigodjurovic@gmail.com>
2020-08-31 20:25:47 +0200

    Prvi rez

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDb = require('./config/db')
+
+const app = express();
+
+connectDb();
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Pozdrav iz zemlje Safari'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/buildings', require('./routes/api/buildings'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started ${PORT}`));
